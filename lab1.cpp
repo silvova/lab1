ffe77701f70c5545c29201d0d50962dd04a97d56
silvova <silvova93@gmail.com>
2020-09-17 23:48:51 +0300

    lab1

---
+++ lab1.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+	float a = 0, b = 0, i = 0, t1 = 0, t2 = 0;
+	printf("Enter 10 positive numbers\n");
+	scanf_s("%f", &b);
+	if (b < 0) {
+		printf("ERROR");
+		return -1;
+	}
+	for (; i < 9; i = i + 1) {
+		t1 = b;
+		scanf_s("%f", &a);
+		if (a < 0) {
+			printf("ERROR");
+			return -1;
+		}
+		t2 = a;
+		while (a >= 1) a = a - 1;
+		while (b >= 1) b = b - 1;
+		if (b < a) b = t1;
+		else b = t2;
+     }
+	printf("%f", b);
+	return 0;
+}
